4853627604d992214a48afe0d527536a5c25f534
somnang tep <163022251+Tsn168@users.noreply.github.com>
2025-11-27 11:04:19 +0700

    im wrong

---
+++ week7/lib/1 - START CODE 6/EXERCISE-4/data/jokes.dart
@@ -0,0 +1,51 @@
+class Joke {
+  final String title;
+  final String description;
+
+  Joke({required this.title, required this.description});
+}
+
+List<Joke> jokes = [
+  Joke(
+    title: "Joke 1",
+    description:
+        "Why don't skeletons fight each other? They don’t have the guts.",
+  ),
+  Joke(
+    title: "Joke 2",
+    description:
+        "I'm reading a book about anti-gravity. It's impossible to put down!",
+  ),
+  Joke(
+    title: "Joke 3",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 4",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 5",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 6",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 7",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 8",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 9",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+  Joke(
+    title: "Joke 10",
+    description: "Why did the bicycle fall? It was two tired.",
+  ),
+];
--- week7/lib/1 - START CODE 6/EXERCISE-4/main.dart
+++ week7/lib/1 - START CODE 6/EXERCISE-4/main.dart
@@ -1,71 +1,99 @@
 import 'package:flutter/material.dart';
+import 'data/jokes.dart';
 
-Color appColor = Colors.green[300] as Color;
-
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: appColor,
-          title: const Text("Favorite Jokes"),
-        ),
-        body: const Column(
-          children: [FavoriteCard(), FavoriteCard(), FavoriteCard()],
-        ),
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text('Favorite jokes', style: TextStyle(color: Colors.white)),
       ),
-    ));
+      body: JokeListScreen(),
+    ),
+  ),
+);
 
-class FavoriteCard extends StatefulWidget {
-  const FavoriteCard({
-    super.key,
-  });
+class JokeListScreen extends StatefulWidget {
+  const JokeListScreen({super.key});
 
   @override
-  State<FavoriteCard> createState() => _FavoriteCardState();
+  State<JokeListScreen> createState() => _JokeListScreenState();
 }
 
-class _FavoriteCardState extends State<FavoriteCard> {
-  bool _isFavorite = false;
+class _JokeListScreenState extends State<JokeListScreen> {
+  int? favoriteIndex; // store which joke is the favorite
 
-  void onFavoriteClick() {
+  void setFavorite(int index) {
     setState(() {
-      _isFavorite = !_isFavorite;
+      if (favoriteIndex == index) {
+        favoriteIndex = null; //click un favorite
+      } else {
+        favoriteIndex = index;
+      }
     });
   }
- 
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: jokes.length,
+      itemBuilder: (context, index) {
+        return FavoriteCard(
+          joke: jokes[index],
+          isFavorite: favoriteIndex == index,
+          onFavoriteClick: () => setFavorite(index),
+        );
+      },
+    );
+  }
+}
+
+class FavoriteCard extends StatelessWidget {
+  final Joke joke;
+  final bool isFavorite;
+  final VoidCallback onFavoriteClick;
+
+  const FavoriteCard({
+    super.key,
+    required this.joke,
+    required this.isFavorite,
+    required this.onFavoriteClick,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        border: Border(
-          bottom: BorderSide(width: .5, color: Colors.grey),
-        ),
+        border: Border(bottom: BorderSide(width: .5, color: Colors.grey)),
       ),
       padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-           Expanded(
+          Expanded(
             flex: 7,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'title',
+                  joke.title,
                   style: TextStyle(
-                      color: appColor, fontWeight: FontWeight.w800),
+                    color: Colors.green,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
-                const SizedBox(height: 10.0),
-                const Text('description')
+                const SizedBox(height: 10),
+                Text(joke.description),
               ],
             ),
           ),
           IconButton(
-              onPressed: onFavoriteClick,
-              icon: Icon(
-                _isFavorite ? Icons.favorite : Icons.favorite_border,
-                color: _isFavorite ? Colors.red : Colors.grey,
-              ))
+            onPressed: onFavoriteClick,
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: isFavorite ? Colors.red : Colors.grey,
+            ),
+          ),
         ],
       ),
     );
